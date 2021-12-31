4ac4a8337ccc485104205f22b68b6084ebf48d88
hkgnp <ben@pngs.cc>
2022-01-01 00:49:33 +0800

    added waiting message when blocks are not inserted

--- src/App.jsx
+++ src/App.jsx
@@ -126,6 +126,20 @@ const App = (props) => {
           />
         </BarChart>
       )}
+
+      {chart === '' && (
+        <div
+          style={{
+            border: '1px solid white',
+            padding: '0.5em',
+            marginTop: '-2rem',
+          }}
+        >
+          Start by indicating the type of chart you want followed by the colour
+          in the child block, e.g. "bar white" or "line pink" or "pie #ffffff"
+          or "area rgb(250,100,80)". Refer to the Readme for more instructions!
+        </div>
+      )}
     </React.Fragment>
   );
 };
